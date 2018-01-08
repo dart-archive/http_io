c103e9bce65f35dc0f9de7ab8c58e52953370829
Kevin Moore <kevmoo@google.com>
2018-01-07 21:37:43 -0800

    oops

--- test/session_test.dart
+++ test/session_test.dart
@@ -3,8 +3,8 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'dart:async';
-import 'dart:io';
 
+import 'package:http_io/http_io.dart';
 import 'package:test/test.dart';
 
 const _sessionId = "DARTSESSID";
